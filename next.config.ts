336c68d5dfbd424638af7aa3c8a7c10d897965da
Princeton Aihebho <letstalk@cofellow.com>
2026-09-20 14:46:42 +0100

    CSP updated

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,18 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 
 const ContentSecurityPolicy = [
   "default-src 'self'",
-  `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://googleads.g.doubleclick.net${
+  `script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://googleads.g.doubleclick.net https://static.cloudflareinsights.com${
     isDevelopment ? " 'unsafe-eval'" : ""
   }`,
-  "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://googleads.g.doubleclick.net",
+  "script-src-elem 'self' 'unsafe-inline' https://www.googletagmanager.com https://googleads.g.doubleclick.net https://static.cloudflareinsights.com",
   "object-src 'none'",
   "base-uri 'self'",
   "frame-src 'none' https://www.googletagmanager.com",
   "frame-ancestors 'none'",
-  "img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://www.google.com https://ad.doubleclick.net",
+  "img-src 'self' data: https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://www.google.com https://www.google.com.ng https://ad.doubleclick.net https://static.cloudflareinsights.com",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' data: https://fonts.gstatic.com",
-  "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://www.google.com https://ad.doubleclick.net https://www.googleadservices.com",
+  "connect-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://googleads.g.doubleclick.net https://www.google.com https://www.google.com.ng https://ad.doubleclick.net https://www.googleadservices.com https://static.cloudflareinsights.com",
 ].join("; ");
 
 const nextConfig: NextConfig = {
